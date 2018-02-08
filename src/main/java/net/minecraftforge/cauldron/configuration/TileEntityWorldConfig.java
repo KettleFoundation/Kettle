9c1672848a6a4b740ccf61cf1583ad84f18fb5fa
aolko <aolko93@gmail.com>
2018-02-08 09:36:56 +0300

    initial java

--- src/main/java/net/minecraftforge/cauldron/configuration/TileEntityWorldConfig.java
+++
@@ -1,9 +0,0 @@
-package net.minecraftforge.cauldron.configuration;
-
-public class TileEntityWorldConfig extends WorldConfig
-{
-    public TileEntityWorldConfig(String worldName, ConfigBase configFile)
-    {
-        super(worldName, configFile);
-    }
-}
