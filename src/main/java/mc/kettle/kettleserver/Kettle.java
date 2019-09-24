c98492153df3543ac09424016fb27ecbe567c8e1
GMatrixGames <gmatrixgames@hotmail.com>
2019-09-24 15:57:13 -0300

    Update some more stuff, idk rn

--- src/main/java/mc/kettle/kettleserver/Kettle.java
+++ src/main/java/mc/kettle/kettleserver/Kettle.java
@@ -1,5 +1,25 @@
 package mc.kettle.kettleserver;
 
+import java.util.logging.Logger;
+
 public class Kettle {
-	
+	private static final String REMAP_NAME = "v1_12_R1";
+	private static final String REMAP_PREFIX = "net/minecraft/server/";
+	public static Logger logger;
+
+	public static String getRemapName() {
+	    return REMAP_NAME;
+    }
+
+    public static String getRemapPrefix() {
+        return REMAP_PREFIX;
+    }
+
+    public static void main(String[] args) {
+	    if (System.getProperty("log4j.configurationFile") == null) {
+	        System.setProperty("log4j.configurationFile", "log4j2.xml");
+        }
+
+
+    }
 }
